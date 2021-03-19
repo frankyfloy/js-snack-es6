481b13d4baddf428ca01e330b18522233544c2a0
Francesco Sica <fsica171@gmail.com>
2021-03-19 17:54:12 +0100

    .

--- assets/main.js
+++ assets/main.js
@@ -9,31 +9,81 @@ $(document).ready(function (){
 // Jsnack gatti
 function Snack3(){
 
+
     const gatti = [];
 
-    const gatto1 = {
-        name : "leo",
-        età : "10",
-        colore : "bianco",
-        sesso : "maschio"
+    var myForm_$ = $("form#my_Form_Gatti");
+
+    myForm_$.submit(function( event ) {
+
+        // Annullamento invio dati (Refresh page)
+        event.preventDefault();
+
+        let my_btn_Input_$ = $("form#my_Form_Gatti > button > input");
+
+
+
+        const gatto = {
+            name: $(my_btn_Input_$).eq(0).val(),
+            età: parseInt($(my_btn_Input_$).eq(1).val()),
+            colore: $(my_btn_Input_$).eq(2).val(),
+            sesso: $(my_btn_Input_$).eq(3).val()
+        };
+
+        gatti.push(gatto)
+
+        // Reset valori input
+        $(my_btn_Input_$).eq(0).val("");
+        $(my_btn_Input_$).eq(1).val("");
+        $(my_btn_Input_$).eq(2).val("");
+        $(my_btn_Input_$).eq(3).val("");
+    });
+
+
+    $("#my_Table_Gatti button").click( function(){
+        console.log(gatti);
+
+        gatti.forEach((element) => {
+            console.log(element.name);
+            add_Dati(element.name,element.colore)
+        });
+
+        const a_gattiM = gatti.filter((element) => {
+        return element.sesso === "maschio";
+        });
+
+        const a_gattiF = gatti.filter((element) => {
+        return element.sesso === "femmina";
+        });
+
+        console.log(a_gattiM);
+        console.log(a_gattiF);
+
+        // TODO: da finire (Stanco)
+
+
+
+
+        // let itemPiuLeggera = `<tr class="text-success bicicletta border border-success"><td class="nome bg-transparent"><strong>${biciclette[indexSuccessivoMin].name}</strong>
+        // </td><td><strong>${biciclette[indexSuccessivoMin].peso}</strong></td></tr>`;
+
+        // $("#tabellaBici").append(itemPiuLeggera);
+        // console.log(biciclette);
+
+        // setTimeout(function(){
+        //     $("#tabellaBici").children().remove();
+        //     biciclette  = [];
+        //     console.log(biciclette);
+        // },3000);
+
+    });
+
+    function add_Dati(nome,colore){
+        let str = `<tr class="text-success border border-dark"><td class="nome"><strong>${nome}</strong>
+        </td><td><strong>${colore}</strong></td></tr>`;
+        $("#tabellaGatti").append(str);
     };
 
-    const gatto2 = {
-        name : "Anna",
-        età : "12",
-        colore : "nero",
-        sesso : "femmina"
-    };
-
-    const gatto3 = {
-        name : "Lia",
-        età : "8",
-        colore : "marrone",
-        sesso : "femmina"
-    };
-
-    gatti.push(gatto1,gatto2,gatto3);
-    console.log(gatti);
 
 }
 
